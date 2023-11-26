3f3a95483c6100685c6b808facc141d5edff1ae2
webaldar <webaldar@gmail.com>
2023-11-26 17:59:10 +0300

    variable create

---
+++ JS/create-decimal-numbers-with-javascript.js
@@ -0,0 +1,11 @@
+// 📌 DESCRIPTION:
+
+//Create a variable myDecimal and give it a decimal value with a fractional part (e.g. 5.7).
+
+
+// 💚 SOLUTIONS
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
